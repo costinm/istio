8f0ed294432745a4c634dd7ff841aa170f03f440
Costin Manolache <costin@gmail.com>
2018-09-20 11:39:13 -0700

    Add load test and fixes

--- tools/adsload/adsload.go
+++ tools/adsload/adsload.go
@@ -24,21 +24,33 @@ package main
 import (
 	"flag"
 	"log"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
 	"time"
 
-	"net"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"istio.io/istio/pkg/adsc"
 )
 
 var (
+	// For local testing (single pilot) - better to keep it bellow 1000
+	// As of Istio1.0.3, with a throttle of 250 qps push time for 1000 is ~15 seconds,
+	// and with 500 qps push time is 28 seconds.
+	//
 	clients = flag.Int("clients",
-		100,
-		"Number of ads clients")
+		1000,
+		"Number of ads clients.")
 
 	pilotAddr = flag.String("pilot",
 		"localhost:15010",
 		"Pilot address. Can be a real pilot exposed for mesh expansion.")
+
+	port = flag.String("port",
+		":14000",
+		"Http port for debug and control.")
 
 	certDir = flag.String("certDir",
 		"", // /etc/certs",
@@ -52,7 +64,39 @@ func main() {
 		n := i
 		go runClient(n)
 	}
-	select {}
+	http.Handle("/metrics", promhttp.Handler())
+	initMetrics()
+	http.ListenAndServe(*port, nil)
+}
+
+var (
+	initialConnectz = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name:    "connect",
+		Help:    "Initial connection time, in ms",
+		Buckets: []float64{100, 500, 1000, 2000, 5000, 10000, 20000, 40000},
+	})
+
+	connectedz = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "connected",
+		Help: "Connected clients",
+	})
+
+	connectTimeoutz = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "connectTimeout",
+		Help: "Connect timeouts",
+	})
+
+	cfgRecvz = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "configs",
+		Help: "Received config types",
+	}, []string{"type"})
+)
+
+func initMetrics() {
+	prometheus.MustRegister(initialConnectz)
+	prometheus.MustRegister(connectedz)
+	prometheus.MustRegister(connectTimeoutz)
+	prometheus.MustRegister(cfgRecvz)
 }
 
 // runClient creates a single long lived connection
@@ -69,18 +113,33 @@ func runClient(n int) {
 
 	c.Watch()
 
+	initialConnect := true
 	_, err = c.Wait("rds", 30*time.Second)
 	if err != nil {
 		log.Println("Timeout receiving RDS")
+		connectTimeoutz.Add(1)
+		initialConnect = false
+	} else {
+		connectedz.Add(1)
 	}
 
-	log.Println("Initial connection: ", time.Since(t0))
+	ctime := time.Since(t0)
+	initialConnectz.Observe(float64(ctime / 1000000)) // ms
 
 	for {
 		msg, err := c.Wait("", 15*time.Second)
 		if err == adsc.ErrTimeout {
 			continue
 		}
+		if msg == "close" {
+			c.Reconnect()
+		}
+		if !initialConnect && msg == "rds" {
+			// This is a delayed initial connect
+			connectedz.Add(1)
+			initialConnect = true
+		}
+		cfgRecvz.With(prometheus.Labels{"type": msg}).Add(1)
 		log.Println("Received ", msg)
 	}
 }
