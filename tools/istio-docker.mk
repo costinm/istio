e8213ebd873dc27929a1b635c4ffc310afb017ab
costin <costin@gmail.com>
2020-09-08 12:58:00 -0700

    Generate prom config

--- tools/istio-docker.mk
+++ tools/istio-docker.mk
@@ -109,13 +109,13 @@ docker.cloudrun: BUILD_PRE=&& chmod 755 pilot-discovery
 docker.cloudrun: BUILD_ARGS=--build-arg BASE_VERSION=${BASE_VERSION}
 docker.cloudrun: $(ISTIO_OUT_LINUX)/pilot-discovery
 docker.cloudrun: $(ISTIO_OUT)/knative/telemetry-sd.yaml
+docker.cloudrun: $(ISTIO_OUT)/knative/telemetry.yaml
 docker.cloudrun: manifests/charts/base/files/gen-istio-cluster.yaml
 docker.cloudrun: manifests/charts/istio-control/istio-discovery/files/injection-template.yaml
 docker.cloudrun: tools/packaging/knative/injection-values.yaml
 docker.cloudrun: tools/packaging/knative/istiod-gcp.sh
 docker.cloudrun: tools/packaging/knative/mesh_template.yaml
 docker.cloudrun: tools/packaging/knative/mutating_template.yaml
-docker.cloudrun: tools/packaging/knative/telemetry.yaml
 docker.cloudrun: tools/packaging/knative/Dockerfile.cloudrun
 	$(DOCKER_RULE)
 
